f5d21e4e07ed87f891bd937234388ead1247c8ad
Danilo Cândido <danilod07@gmail.com>
2018-02-06 00:36:20 -0300

    adding css styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <div class="painel">
+          <h2 class="painel-titulo">{{foto.titulo}}</h2>
+          <div class="painel-conteudo">
+            <img class="image-responsiva" :src="foto.url" :alt="foto.titulo">
+          </div>
+        </div>
       </li>
     </ul>
+
   </div>
 </template>
 
@@ -22,7 +29,7 @@ export default {
 
   created() {
     let promisse = this.$http.get('http://localhost:3000/v1/fotos');
-    
+
     promisse
       .then(response => response.json())
       .then(fotos => this.fotos = fotos);
@@ -32,5 +39,48 @@ export default {
 </script>
 
 <style>
+  .centralizado {
+    text-align: center;
+  }
 
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .image-responsiva {
+    width: 100%;
+  }
+
+  /* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
