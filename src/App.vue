5927ba9f418cfd309883c51a1179adb027187786
Danilo Cândido <danilod07@gmail.com>
2018-03-01 09:35:31 -0300

    register vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,19 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título">
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <img class="image-responsiva" :src="foto.url" :alt="foto.titulo">
-        </meu-painel>
-      </li>
-    </ul>
+    <!-- Hora tem que exibir um componente, hora outro usando as Rotas -->
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-
 export default {
 
-  components: {
-    'meu-painel': Painel
-  },
-
-  data() {
-    return {
-      titulo: 'Alurapic',
-      fotos: [],
-      filtro: ''
-    }
-  },
-
-  computed: {
-    fotosComFiltro(){
-      if(this.filtro){
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo))
-      }else{
-        return this.fotos;
-      }
-    }
-  },
-
-  created() {
-    let promisse = this.$http.get('http://localhost:3000/v1/fotos');
-
-    promisse
-      .then(response => response.json())
-      .then(fotos => this.fotos = fotos);
-  }
 }
-
 </script>
 
 <style>
-  .centralizado {
-    text-align: center;
-  }
-
   .corpo {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
-  }
-
-  .lista-fotos {
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .image-responsiva {
-    width: 100%;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
   }
 </style>
