0bb0ae3a59d39df6886bfb3faf12138a7458e0dd
Danilo Cândido <danilod07@gmail.com>
2018-03-01 21:39:22 -0300

    route from routes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="corpo">
+    <nav>
+      <ul>       
+          <li v-for="route in routes">
+            <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
+          </li>
+      </ul>
+    </nav>
+
     <!-- Hora tem que exibir um componente, hora outro usando as Rotas -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { routes } from './routes';
+
 export default {
+
+  data() {
+    return {
+      routes
+    }
+  }
 
 }
 </script>
